a6b1e1009c6325f30daaf446759aaf22497da3e1
pamVarela <varelapamela6@gmail.com>
2021-01-24 21:09:27 -0600

    frotend mockup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table'; 
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +23,14 @@ import { MatTableModule } from '@angular/material/table';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatTableModule,
+    MatIconModule,
+    FontAwesomeModule,
+    MatButtonModule,
+    MatExpansionModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPlus,faPencilAlt} from '@fortawesome/free-solid-svg-icons';
+import {  faTrashAlt  } from '@fortawesome/free-regular-svg-icons';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  faPlus = faPlus; faTrash = faTrashAlt; faPencil = faPencilAlt;
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
+  columns: string[] = ['Nombre', 'Categoría', 'Precio', 'Cantidad', 'Inventario', 'Acción'];
+
+  dataSource = new MatTableDataSource([
+    {position: 'Nike', name: 'Hydrogen', weight: 1.0079, symbol: 1},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 2},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 0},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 0},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 1},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 2},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 0},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 2},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 1},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 0},
+    
+  ]);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   constructor() { }
 
